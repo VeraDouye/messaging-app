b7e3e8fbe46a00c0d9701c338a04a74d73523d35
vera douye <veralloydekpebu@gmail.com>
2020-12-26 00:08:04 +0100

    initial commit

---
+++ src/components/Users.js
@@ -0,0 +1,39 @@
+export const dummyUsers = [
+    {
+    id: "2",
+    name: "Kachi Samson",
+    email: "kasam@sample.com",
+    photoUrl: "https://images.unsplash.com/photo-1529758146491-1e11fd721f77?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OHx8cHJvZmlsZSUyMGFmcmljYW58ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
+    role: "Member",
+    info: "Product Designer at Facebook",
+    welcomeMessage: "Hey there! Love to chat :-)"
+    },
+    {
+    id: "3",
+    name: "Ebere Abudu",
+    email: "aebere@sample.com",
+    photoUrl: "https://images.unsplash.com/photo-1531300185372-b7cbe2eddf0b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fHByb2ZpbGUlMjBhZnJpY2FufGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
+    role: "Member",
+    info: "Digital Marketer at Microsoft",
+    welcomeMessage: "Hey there! Love to chat :-)"
+    },
+    {
+    id: "4",
+    name: "Grace Joshua",
+    email: "grace@sample.com",
+    photoUrl: "https://images.unsplash.com/photo-1529123202249-4f6224196c9b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTR8fHByb2ZpbGUlMjBhZnJpY2FufGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
+    role: "Member",
+    info: "Product Designer at Google",
+    welcomeMessage: "Hey there! Love to chat :-)"
+    },
+    {
+    id: "5",
+    name: "Angie Godson",
+    email: "angel@sample.com",
+    photoUrl: "https://pbs.twimg.com/profile_images/834493671785525249/XdLjsJX_.jpg",
+    role: "Member",
+    info: "Software Engineer at TalkJS",
+    welcomeMessage: "Hey there! Love to chat :-)"
+    },
+    ]
+    
